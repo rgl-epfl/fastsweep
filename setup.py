2028e406a30e9c225ff721e4811cb09ef8a1fc73
Delio Vicini <delio.vicini@gmail.com>
2022-07-18 18:12:12 +0200

    Work on fixing arm64 build

--- setup.py
+++ setup.py
@@ -16,6 +16,9 @@ this_directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+fastsweep_cmake_toolchain_file = os.environ.get("FASTSWEEP_CMAKE_TOOLCHAIN_FILE", "")
+fastsweep_drjit_cmake_dir = os.environ.get("FASTSWEEP_DRJIT_CMAKE_DIR", "")
+
 setup(
     name="fastsweep",
     version="1.0.1",
@@ -28,6 +31,11 @@ setup(
     packages=find_packages(where="src"),
     install_requires=["drjit"],
     package_dir={"": "src"},
+    cmake_args=[
+        f'-DCMAKE_TOOLCHAIN_FILE={fastsweep_cmake_toolchain_file}',
+        f'-DDRJIT_CMAKE_DIR:STRING={fastsweep_drjit_cmake_dir}'
+    ],
+
     cmake_install_dir="src/fastsweep",
     include_package_data=True,
     python_requires=">=3.8",
